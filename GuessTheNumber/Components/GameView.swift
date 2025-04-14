f5d2f16e20f51da13cbd4aecf142bf1ebd72a022
Morgan Pritchard <morgan.pritchard@nwktc.edu>
2025-04-14 08:04:09 -0600

    Finished Guess The Number

---
+++ GuessTheNumber/Components/GameView.swift
@@ -0,0 +1,18 @@
+//
+//  GameView.swift
+//  GuessTheNumber
+//
+//  Created by Morgan Pritchard on 4/14/25.
+//
+
+import SwiftUI
+
+struct GameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameView()
+}
---
+++ GuessTheNumber/Components/Guesses.swift
@@ -0,0 +1,18 @@
+//
+//  Guesses.swift
+//  GuessTheNumber
+//
+//  Created by Morgan Pritchard on 4/11/25.
+//
+
+import SwiftUI
+
+struct Guesses: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Guesses()
+}
